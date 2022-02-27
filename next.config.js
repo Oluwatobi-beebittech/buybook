fc944eca0ba9da48e80c3a77db638f4ab3df0eaf
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-02-27 17:12:39 +0100

    added: less usage to app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withLess = require("next-with-less");
+
+const nextConfig = withLess({
   reactStrictMode: true,
-}
+  lessLoaderOptions: {
+    options: {
+      sourceMap: true,
+    },
+  }
+})
 
 module.exports = nextConfig
