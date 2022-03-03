17c5a605194a6176cacd9199e56af44c4e97fdde
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-03 22:33:07 +0100

    fix: eslint automatic linting on save

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ const withLess = require("next-with-less");
 
 const nextConfig = withLess({
   reactStrictMode: true,
-  lessLoaderOptions: {
-    options: {
-      sourceMap: true,
-    },
-  }
+  lessLoaderOptions: {}
 })
 
 module.exports = nextConfig
