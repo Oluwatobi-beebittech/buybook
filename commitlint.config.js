75cc4b8cabe86171d5b78bff274041414c6e3191
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-03 21:33:45 +0100

    chore: eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,17 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'added',
-          'updated',
-          'removed',
-          'fixed',
-          'wip',
-        ],
-      ],
-    },
+    // rules: {
+    //   'type-enum': [
+    //     2,
+    //     'always',
+    //     [
+    //       'added',
+    //       'updated',
+    //       'removed',
+    //       'fixed',
+    //       'wip',
+    //     ],
+    //   ],
+    // },
   };
   
