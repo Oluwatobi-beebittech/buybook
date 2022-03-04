8207b651e327b073f8d4604e49871c42a4ba8c9c
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-04 22:24:41 +0100

    chore: updated pages folder name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from 'next/app';
+import '@styles/globals.css';
+import '@styles/main.less';
 
-import '../styles/globals.css';
-import '../Styles/main.less ';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import styles from '@styles/Home.module.css';
 import Head from 'next/head';
 import Image from 'next/image';
 
 import type { NextPage } from 'next';
 
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
