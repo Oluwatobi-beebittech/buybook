08688a759778007d37959c21f75118d910cd2f2e
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-12 22:47:37 +0100

    feat: navigation bar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,25 +3,23 @@ import * as React from 'react';
 
 export const Navigation = () => (
 	<nav className = "nav">
-		<div>
-			<h1>Buy Book</h1>
-			<ul>
-				<li className="nav__link">
-					<Link href='/'>
-						<a>	About</a>
-					</Link>
-				</li>
-				<li className="nav__link">
-					<Link href='/'>
+		<span className="nav__header">Buy Book</span>
+		<ul>
+			<li className="nav__link">
+				<Link href="/">
+					<a>	About</a>
+				</Link>
+			</li>
+			<li className="nav__link">
+				<Link href="/">
 				Register
-					</Link>
-				</li>
-				<li className="nav__link">
-					<Link href='/'>
+				</Link>
+			</li>
+			<li className="nav__link">
+				<Link href="/">
 				Login
-					</Link>
-				</li>
-			</ul>
-		</div>
+				</Link>
+			</li>
+		</ul>
 	</nav>
 );
