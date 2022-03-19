31c4b94cdf0817bf0792d642361adb6e739b25fd
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-19 22:27:09 +0100

    chore: styled landing page hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Space, Typography } from 'antd';
+import { Button, Col, Row, Space, Typography } from 'antd';
 import Head from 'next/head';
 
 import { HandCoinsIcon, ReusedBookIcon } from '@Assets';
@@ -8,7 +8,7 @@ import type { TypographyProps } from 'antd';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	const { Title }: Pick<TypographyProps, 'Title'> = Typography;
+	const { Paragraph, Title }: Pick<TypographyProps, 'Paragraph'|'Title'> = Typography;
 
 	return (
 		<>
@@ -24,23 +24,24 @@ const Home: NextPage = () => {
 					<Row gutter={[ 0,0 ]}>
 						<Col span={24} className="landing__card--large">
 							<Title>Get Books with Ease</Title>
-							<p>Helo</p>
+							<Paragraph>Discover book lovers and get access to their collection of books.</Paragraph>
+							<Button type="primary" size="large">Explore</Button>
 						</Col>
 					</Row>
 					<Row gutter={[ 20, 20 ]}>
 						<Col span={4} className="landing__card--small">
-							<ReusedBookIcon style={{ fontSize: '3rem' }}/>
+							<ReusedBookIcon />
 						</Col>
 						<Col span={20} className="">
-							<Title level={2}>Lend a book</Title>
+							<Title level={3}>Lend a book</Title>
 						</Col>
 					</Row>
 					<Row gutter={[ 20, 20 ]}>
 						<Col span={4} className="landing__card--small">
-							<HandCoinsIcon style={{ fontSize: '3rem' }}/>
+							<HandCoinsIcon />
 						</Col>
 						<Col span={20} className="">
-							<Title level={2}>Purchase a used copy</Title>
+							<Title level={3}>Purchase a used copy</Title>
 						</Col>
 					</Row>
 				</Space>
