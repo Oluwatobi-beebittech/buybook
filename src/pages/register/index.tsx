2cd13610e304a52a256c888e8d92003b4f722e1a
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-23 23:01:57 +0100

    feat: added landing page links to pages

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import * as React from 'react';
+
+import { Navigation } from '@Components';
+
+import type { NextPage } from 'next';
+
+const Register: NextPage = (): JSX.Element => (
+	<>
+		<Head>
+			<title>Register</title>
+			{/* <link rel="icon" href="/favicon.ico" /> */}
+		</Head>
+		<header>
+			<Navigation/>
+		</header>
+	</>
+);
+
+export default Register;
