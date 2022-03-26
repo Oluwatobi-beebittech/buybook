beb12079249a9900a460098e4f10f737fd70e937
Oluwatobi Akanji <Oluwatobi-beebittech@users.noreply.github.com>
2022-03-26 14:53:10 +0100

    test: added navigation link presence tests (#43)

--- src/Components/Navigation/Navigation.test.tsx
+++ src/Components/Navigation/Navigation.test.tsx
@@ -1,11 +1,36 @@
-import { render } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
+import * as React from "react";
+
+import type { LinkProps } from "next/link";
 
 import { Navigation } from "./Navigation";
 
+jest.mock("next/link", () => (
+	{ children }: React.PropsWithChildren<LinkProps>
+)=> children
+);
+
 describe("navigation", () => {
-	it("renders without crashing", () => {
-		const { getByRole } = render(<Navigation/>);
-		expect(getByRole("navigation")).toBeDefined();
+	beforeAll(() => {
+		render(<Navigation/>);
 	});
 
+	afterAll(() => {
+		cleanup();
+	});
+
+	it("renders without crashing", () => {
+		const navigationBar = screen.getByRole("navigation");
+		expect(navigationBar).toBeDefined();
+	});
+
+	test("links in navigation are present and clickable", () => {
+		const aboutLink = screen.getByText("About");
+		const loginLink = screen.getByText("Login");
+		const registerLink = screen.getByText("Register");
+
+		expect(aboutLink).toBeInTheDocument();
+		expect(loginLink).toBeInTheDocument();
+		expect(registerLink).toBeInTheDocument();
+	});
 });
