3bf89fee1cc3782bc034be339432294417ece46a
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-03-30 21:12:10 +0100

    chore: updated login page layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,15 @@
-import { LoginOutlined } from '@ant-design/icons';
-import {  Button, Checkbox, Col ,Form, Input, Row, Typography } from 'antd';
-import Head from 'next/head';
-import * as React from 'react';
+import { LoginOutlined , UserOutlined, LockOutlined } from "@ant-design/icons";
+import {  Button, Checkbox, Col ,Form, Input, Row, Typography } from "antd";
+import Head from "next/head";
+import * as React from "react";
 
-import { Navigation } from '@Components';
+import { Navigation } from "@Components";
 
-import type { TypographyProps } from 'antd';
-import type { NextPage } from 'next';
+import type { TypographyProps } from "antd";
+import type { NextPage } from "next";
 
 const Login: NextPage = (): JSX.Element => {
-	const { Title }: Pick<TypographyProps, 'Title'> = Typography;
+	const { Title }: Pick<TypographyProps, "Title"> = Typography;
 
 	return (
 		<>
@@ -22,10 +22,11 @@ const Login: NextPage = (): JSX.Element => {
 			</header>
 			<main className="login__content">
 				<Row justify="space-around" align="middle" className="login__content-row">
-					<Col span={12} className="login__content-container">
+					<Col span={24} md={12} className="login__content-container">
 						<Title level={3}><LoginOutlined /> Login</Title>
 						<Form
 							name="login"
+							colon={false}
 							labelCol={{ span: 6 }}
 							wrapperCol={{ span: 16 }}
 							initialValues={{ remember: true }}
@@ -36,25 +37,22 @@ const Login: NextPage = (): JSX.Element => {
 							<Form.Item
 								label="Email"
 								name="email"
-								rules={[ { required: true, message: 'Please input your email!' } ]}
+								rules={[ { required: true, message: "Please input your email!", whitespace: true } ]}
 							>
-								<Input />
+								<Input prefix={<UserOutlined style={{ color: "#8c8c8c" }} />} placeholder="Email" size="large"/>
 							</Form.Item>
-
 							<Form.Item
 								label="Password"
 								name="password"
-								rules={[ { required: true, message: 'Please input your password!' } ]}
+								rules={[ { required: true, message: "Please input your password!" } ]}
 							>
-								<Input.Password />
+								<Input.Password prefix={<LockOutlined style={{ color: "#8c8c8c" }}/>}  placeholder="Password" size="large"/>
 							</Form.Item>
-
 							<Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
 								<Checkbox>Remember me</Checkbox>
 							</Form.Item>
-
-							<Form.Item wrapperCol={{ offset: 12, span: 3 }}>
-								<Button type="primary" htmlType="submit">
+							<Form.Item wrapperCol={{ span: 3, offset: 6 }}>
+								<Button type="primary" htmlType="submit" size="large">
           							Submit
 								</Button>
 							</Form.Item>
