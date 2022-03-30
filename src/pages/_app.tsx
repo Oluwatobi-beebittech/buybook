8b21bcb549360a53974ea9c9dfc259f5b87a5071
Oluwatobi Akanji <Oluwatobi-beebittech@users.noreply.github.com>
2022-03-30 21:23:35 +0100

    Chore/login page 1 (#49)
    * chore: updated login page layout
    
    * chore: updated double quotes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@styles/main.less';
+import "@styles/main.less";
 
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
