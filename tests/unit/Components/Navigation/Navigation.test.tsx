5edc388439bb1b7d04327da74ad4f42e11d7f04e
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-04-04 20:55:58 +0100

    test: moved unit tests to test folder

--- tests/unit/Components/Navigation/Navigation.test.tsx
+++ tests/unit/Components/Navigation/Navigation.test.tsx
@@ -1,7 +1,7 @@
 import { cleanup, render, screen } from '@testing-library/react';
 import * as React from 'react';
 
-import { Navigation } from './Navigation';
+import { Navigation } from '@Components';
 
 describe('navigation', () => {
 	beforeAll(() => {
